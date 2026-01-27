9501d81d0a519278db65f9b37e45216b2a238b75
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-01-27 23:51:44 +0530

    created a simple webpage for Ticket Booking site

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>Online Ticket Booker 🎟️</h1>
+      <p>Book tickets easily for movies, events, and travel.</p>
+
+      <div className="ticket-box">
+        Ticket booking system will be available here
+      </div>
+
+      <button>Book Tickets</button>
+    </div>
+  );
+}
+
+export default App;
